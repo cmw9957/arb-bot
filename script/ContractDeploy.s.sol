da3c6fa05ef64a06efaaf832fe80047d06063421
Minwoo_Cho <cmw9957@gmail.com>
2026-01-04 18:14:28 +0900

    feat deploy script

--- script/ContractDeploy.s.sol
+++ script/ContractDeploy.s.sol
@@ -1,24 +1,24 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.27;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
 
-// import {Script} from "forge-std/Script.sol";
-// import {ArbitrageBot} from "../src/Arbitrage.sol";
-// import "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+import {ArbitrageBot} from "../src/Arbitrage.sol";
+import "forge-std/console.sol";
 
-// contract ContractDeploy is Script {
-//     ArbitrageBot public bot;
+contract ContractDeploy is Script {
+    ArbitrageBot public bot;
 
-//     function setUp() public {}
+    function setUp() public {}
 
-//     function run() public {
-//         vm.startBroadcast();
+    function run() public {
+        vm.startBroadcast();
 
-//         bot = new ArbitrageBot();
+        bot = new ArbitrageBot();
 
-//         console.log("===========================================");
-//         console.log("ArbitrageBot deployed at:", address(bot));
-//         console.log("===========================================");
+        console.log("===========================================");
+        console.log("ArbitrageBot deployed at:", address(bot));
+        console.log("===========================================");
 
-//         vm.stopBroadcast();
-//     }
-// }
+        vm.stopBroadcast();
+    }
+}
